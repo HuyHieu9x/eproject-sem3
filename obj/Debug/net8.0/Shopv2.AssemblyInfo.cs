4ea7e39bde51212071b6a15251ea30aa7a7d152e
huent11052001 <hue2193115@gmail.com>
2026-05-30 14:00:18 +0700

    merge code

--- obj/Debug/net8.0/Shopv2.AssemblyInfo.cs
+++ obj/Debug/net8.0/Shopv2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shopv2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0610748e187bd53074d0c49e9d105fb0c203fb4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shopv2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shopv2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
